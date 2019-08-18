8691bb25a1d08c6ddd3358227d9fb7742635f2cc
mesotokyo <meso@meso.tokyo>
2019-08-18 22:52:17 +0900

    implement static-rendering system

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = connect();
 const serveStatic = require('serve-static');
 
 const serverSideRenderer = require('./serverSideRenderer');
+const staticGenerator = require('./static-generator');
 
 // use json parser
 app.use(bodyParser.json({ type: 'application/json' }));
@@ -28,7 +29,7 @@ app.use(router);
 
 // static files
 const accessToken = config.gamebattle.token;
-app.use(`/gamebattle/${accessToken}/edit/`, serveStatic(path.join(__dirname, 'public'), { index: "index.html" }));
+app.use(`/gamebattle/${accessToken}/edit/`, serveStatic(path.join(__dirname, 'private/gamebattle'), { index: "index.html" }));
 
 // server side rendering
 /*
@@ -36,7 +37,7 @@ app.use('/gamebattle/', serverSideRenderer({ templateDir: "./template",
                                              index: "index.html"
                                            }));
 */
-app.use(`/gamebattle/`, serveStatic(path.join(__dirname, 'template'), { index: "index.html" }));
+app.use(`/gamebattle/`, serveStatic(path.join(__dirname, 'public/gamebattle'), { index: "index.html" }));
 
 
 // root
@@ -47,7 +48,11 @@ app.use(function(req, res){
   res.statusCode = 404;
   res.end('Not Found\n');
 });
- 
+
+// static renderings
+console.log("rendering static files...");
+staticGenerator.generate(config);
+
 //create node.js http server and listen on port
 console.log("listen on :3000");
 http.createServer(app).listen(3000);
---
+++ static-generator.js
@@ -0,0 +1,77 @@
+const ejs = require('ejs');
+const fs = require('fs');
+const path = require('path');
+
+const publicDir = path.join(__dirname, "public");
+const privateDir = path.join(__dirname, "private");
+const templateDir = path.join(__dirname, "template");
+
+const templateRegExp = '\.html$';
+
+exports.generate = function generate(config) {
+  for (const eventId in config) {
+    generatePage(eventId, config[eventId]);
+  }
+};
+
+function generatePage(eventId, eventConfig) {
+  const vars = Object.assign(eventConfig);
+  delete vars.database;
+  vars.eventId = eventId;
+
+  // create output directories
+  const publicTargetDir = path.join(publicDir, eventId);
+  let err;
+  try {
+    err = fs.mkdirSync(publicTargetDir);
+  } catch (err) {
+    if (err.code != 'EEXIST') {
+      throw new Error(`cannot create static file directory: ${publicTargetDir}`);
+    }
+  }
+  const privateTargetDir = path.join(privateDir, eventId);
+  try {
+    err = fs.mkdirSync(privateTargetDir);
+  } catch (err) {
+    if (err.code != 'EEXIST') {
+      throw new Error(`cannot create static file directory: ${privateTargetDir}`);
+    }
+  }
+
+  // read templates and render
+  const filenames = fs.readdirSync(templateDir);
+  if (filenames === undefined) {
+    throw new Error(`cannot open template directory: ${templateDir}`);
+  }
+
+  const checkTemplateRex = new RegExp(templateRegExp);
+  const ejsOptions = { strict: true };
+  for (const filename of filenames) {
+    if (!checkTemplateRex.test(filename)) {
+      continue;
+    }
+    const templateStr = fs.readFileSync(path.join(templateDir, filename),
+                                     { encoding: "utf8" });
+    if (templateStr === undefined) {
+      throw new Error(`cannot loadn template file: ${path.join(templateDir, filename)}`);
+    }
+
+    const template = ejs.compile(templateStr, ejsOptions);
+    
+    // render public page
+    vars.target = "public";
+    vars.readOnly = "true";
+    _renderPage(vars, template, path.join(publicTargetDir, filename));
+
+    // render private page
+    vars.target = "private";
+    vars.readOnly = "false";
+    _renderPage(vars, template, path.join(privateTargetDir, filename));
+  }
+  
+}
+
+function _renderPage(vars, template, outputPathname) {
+  const result = template(vars);
+  fs.writeFileSync(outputPathname, result);
+}
